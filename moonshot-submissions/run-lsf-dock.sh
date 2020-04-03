385a10c2580261abdefe9b72b4e423b7771cdff8
John Chodera <john.chodera@choderalab.org>
2020-04-03 08:35:05 -0400

    Add initial docking of round 2 of compounds (minus some very large molecules that didn't finish in time)

--- moonshot-submissions/run-lsf-dock.sh
+++ moonshot-submissions/run-lsf-dock.sh
@@ -8,7 +8,7 @@
 #BSUB -R "span[ptile=1]"
 #BSUB -q cpuqueue
 #BSUB -o %J.moonshot-dock.out
-#BSUB -J "moonshot-dock[1-2386]"
+#BSUB -J "moonshot-dock[1-3564]"
 
 echo "Job $JOBID/$NJOBS"
 
@@ -19,4 +19,5 @@ source ~/.bashrc
 source activate perses
 
 let JOBID=$LSB_JOBINDEX-1
-python ../scripts/02-dock-and-prep.py --molecules covid_submissions_03_31_2020.csv --index $JOBID --output covid_submissions_03_31_2020 --userfrags
+#python ../scripts/02-dock-and-prep.py --molecules covid_submissions_03_31_2020.csv --index $JOBID --output covid_submissions_03_31_2020 --userfrags
+python ../scripts/02-dock-and-prep.py --molecules covid_submissions_with_warhead_info.csv --index $JOBID --output covid_submissions_with_warhead_info --userfrags
