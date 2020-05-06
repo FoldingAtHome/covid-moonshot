4559a86a067e224775955bdec8d863870ac05fc4
John Chodera <john.chodera@choderalab.org>
2020-05-05 23:37:37 -0400

    Clean up of old files

--- moonshot-submissions/run-lsf-dock.sh
+++ moonshot-submissions/run-lsf-dock.sh
@@ -8,7 +8,7 @@
 #BSUB -R "span[ptile=1]"
 #BSUB -q cpuqueue
 #BSUB -o %J.moonshot-dock.out
-#BSUB -J "moonshot-dock[1-4373]"
+#BSUB -J "moonshot-dock[1-4685]"
 
 echo "Job $JOBID/$NJOBS"
 
@@ -18,13 +18,7 @@ source ~/.bashrc
 
 source activate perses
 
-#export PREFIX="covid_submissions_all_info"
-#export PREFIX="covalent_warhead_df"
-#export PREFIX="nir-london-2020-03-07"
-#export PREFIX="covid_submissions_all_info-2020-04-06"
-#export PREFIX="2020_04_07_Nir_covalent_filtered_and_rejects_Holly_7_April"
-#export PREFIX="COVID_MS_final_selection_round_2"
-export PREFIX="covid_submissions_all_info-2020-04-23"
+export PREFIX="covid_submissions_all_info"
 
 let JOBID=$LSB_JOBINDEX-1
-python ../scripts/02-dock-and-prep.py --receptors ../receptors/monomer --molecules $PREFIX.csv --index $JOBID --output $PREFIX-docked --userfrags
+python ../scripts/02-dock-and-prep.py --receptors ../receptors/monomer --molecules $PREFIX.csv --index $JOBID --output $PREFIX-docked 
