093e81fc97e08a36d68cb10cb9f90e4be24617e7
John Chodera <john.chodera@choderalab.org>
2020-08-14 14:31:37 -0400

    Add sprint 2 files

--- synthetic-enumeration/create-json.py
+++ synthetic-enumeration/create-json.py
@@ -6,17 +6,24 @@ from openeye import oechem
 
 ff = 'openff-1.2.0'
 ligand_dict = {
+    # Sprint 1
+
     #'primary_amine_enumeration' : 'primary_amine_enumeration_for_chodera_lab_FEP-permuted-conformers-x10789.sdf',
     #'boronic_ester_enumeration' : 'boronic_ester_enumeration_for_chodera_lab_FEP-permuted-conformers-x10789.sdf',
     #'retrospective-aminopyridines' : 'activity-data-2020-07-29-conformers-x10789.sdf',
     #'retrospective-aminopyridines-matt' : 'aminopyridine_compounds_for_FEP_benchmarking-conformers-x10789.sdf',
     #'retrospective-aminopyridines-matt-dockscores' : 'aminopyridine_compounds_for_FEP_benchmarking-dockscores-x10789.sdf',
     #'fastgrant-table1' : 'fastgrant-table1-dockscores-x10789.sdf',
-    #'RAL-THA-6b94ceba' : 'RAL-THA-6b94ceba-dockscores-x10789.sdf',
-    'EDG-MED-0da5ad92' : 'EDG-MED-0da5ad92-dockscores-x10789.sdf',
+    # 'RAL-THA-6b94ceba' : 'RAL-THA-6b94ceba-dockscores-x10789.sdf',
+
+    # Sprint 2
+    'nucleophilic-displacement' : 'nucleophilic_displacement_enumeration_for_FEP-sorted-x10789.sdf',
 }
+
+json_filename = '2020-08-14-nucleophilic-displacement.json'
+
 receptors = [
-    '../receptors/monomer/Mpro-x2646_0_bound-protein.pdb',
+    #'../receptors/monomer/Mpro-x2646_0_bound-protein.pdb',
     '../receptors/monomer/Mpro-x2646_0_bound-protein-thiolate.pdb'
 ]
 
@@ -27,10 +34,15 @@ receptors = [
 #index = 8086 # starting index for retrospective-aminopyridines-matt
 #index = 8374 # starting index for fastgrant-table1
 #index = 8410 # starting index for RAL-THA-6b94ceba
-index = 8474 # starting index for EDG-MED-0da5ad92
+
+# Sprint 2
+index = 0 # starting index for nucleophilic displacement series
 
 mol_i = oechem.OEGraphMol()
 mol_j = oechem.OEGraphMol()
+
+forwards = False
+backwards = True
 
 i = 0 # reference molecule
 master_dict = dict()
@@ -54,55 +66,57 @@ for series in ligand_dict:
             moldb.GetMolecule(mol_j, j)
 
             # Add forwards
-            master_dict[index] = {
-                'JOBID':index,
-                'directory':f'RUN{index-1}',
-                'series':series,
+            if forwards:
+                master_dict[index] = {
+                    'JOBID':index,
+                    'directory':f'RUN{index-1}',
+                    'series':series,
 
-                'target':'SARS-CoV-2 Mpro',
+                    'target':'SARS-CoV-2 Mpro',
 
-                'start':i,
-                'start_title':mol_i.GetTitle(),
-                'start_smiles':oechem.OECreateSmiString(mol_i, smiles_flag),
+                    'start':i,
+                    'start_title':mol_i.GetTitle(),
+                    'start_smiles':oechem.OECreateSmiString(mol_i, smiles_flag),
 
-                'end':j,
-                'end_title':mol_j.GetTitle(),
-                'end_smiles':oechem.OECreateSmiString(mol_j, smiles_flag),
+                    'end':j,
+                    'end_title':mol_j.GetTitle(),
+                    'end_smiles':oechem.OECreateSmiString(mol_j, smiles_flag),
 
-                'protein':protein,
-                'ligand':ligand_dict[series],
-                'ff':ff,
-            }
-            if oechem.OEHasSDData(mol_i,'f_avg_pIC50'):
-                master_dict[index]['start_pIC50'] = oechem.OEGetSDData(mol_i, 'f_avg_pIC50')
-            if oechem.OEHasSDData(mol_j,'f_avg_pIC50'):
-                master_dict[index]['end_pIC50'] = oechem.OEGetSDData(mol_j, 'f_avg_pIC50')
-            index += 1
+                    'protein':protein,
+                    'ligand':ligand_dict[series],
+                    'ff':ff,
+                    }
+                if oechem.OEHasSDData(mol_i,'f_avg_pIC50'):
+                    master_dict[index]['start_pIC50'] = oechem.OEGetSDData(mol_i, 'f_avg_pIC50')
+                if oechem.OEHasSDData(mol_j,'f_avg_pIC50'):
+                    master_dict[index]['end_pIC50'] = oechem.OEGetSDData(mol_j, 'f_avg_pIC50')
+                index += 1
 
             # Add backwards
-            master_dict[index] = {
-                'JOBID':index,
-                'directory':f'RUN{index-1}',
+            if backwards:
+                master_dict[index] = {
+                    'JOBID':index,
+                    'directory':f'RUN{index-1}',
+                    
+                    'target':'SARS-CoV-2 Mpro',
 
-                'target':'SARS-CoV-2 Mpro',
+                    'start':j,
+                    'start_title':mol_j.GetTitle(),
+                    'start_smiles':oechem.OECreateSmiString(mol_j, smiles_flag),
 
-                'start':j,
-                'start_title':mol_j.GetTitle(),
-                'start_smiles':oechem.OECreateSmiString(mol_j, smiles_flag),
+                    'end':i,
+                    'end_title':mol_i.GetTitle(),
+                    'end_smiles':oechem.OECreateSmiString(mol_i, smiles_flag),
 
-                'end':i,
-                'end_title':mol_i.GetTitle(),
-                'end_smiles':oechem.OECreateSmiString(mol_i, smiles_flag),
+                    'protein':protein,
+                    'ligand':ligand_dict[series],
+                    'ff':ff,
+                 }
+                if oechem.OEHasSDData(mol_j,'f_avg_pIC50'):
+                    master_dict[index]['start_pIC50'] = oechem.OEGetSDData(mol_j, 'f_avg_pIC50')
+                if oechem.OEHasSDData(mol_i,'f_avg_pIC50'):
+                    master_dict[index]['end_pIC50'] = oechem.OEGetSDData(mol_i, 'f_avg_pIC50')
+                index += 1
 
-                'protein':protein,
-                'ligand':ligand_dict[series],
-                'ff':ff,
-            }
-            if oechem.OEHasSDData(mol_j,'f_avg_pIC50'):
-                master_dict[index]['start_pIC50'] = oechem.OEGetSDData(mol_j, 'f_avg_pIC50')
-            if oechem.OEHasSDData(mol_i,'f_avg_pIC50'):
-                master_dict[index]['end_pIC50'] = oechem.OEGetSDData(mol_i, 'f_avg_pIC50')
-            index += 1
-
-with open(f"2020-08-13-EDG-MED-0da5ad92.json", "w") as f:
+with open(json_filename, "w") as f:
     json.dump(master_dict, f, sort_keys=True, indent=4, separators=(',', ': '))
--- synthetic-enumeration/submit-ligpairs.sh
+++ synthetic-enumeration/submit-ligpairs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #BSUB -P "testing"
-##BSUB -J "mpro[1688-4663]" 
-##BSUB -J "mpro[1686-1687]" 
-##BSUB -J "mpro[4664-7641]" 
-##BSUB -J "mpro[7642-7801]" 
-##BSUB -J "mpro[7802-8085]" 
-##BSUB -J "mpro[8085-8373]" 
-##BSUB -J "mpro[8373-8410]" 
-##BSUB -J "mpro[8410-8474]" 
-#BSUB -J "mpro[8474-8542]" 
+#BSUB -J "mpro[1-8428]" 
 #BSUB -n 1
 #BSUB -R rusage[mem=3]
 #BSUB -R span[hosts=1]
